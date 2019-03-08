26fda377a25bca97d29a86b63c6b303e23a84564
Linigari-Luke-216307639 <46363148+Linigari-Luke-216307639@users.noreply.github.com>
2019-03-08 07:38:36 -0800

    finished the starships display functionality

--- src/components.js/Display.js
+++ src/components.js/Display.js
@@ -25,7 +25,18 @@ function Display(props) {
       </div>
     );
   } else if (props.thing) {
-    return <p>thing</p>;
+    return (
+      <div>
+        <p>You have chosen the {props.shipName}</p>
+        <p>Consumables: {props.food} months worth</p>
+        <p>Cost {props.cost} cradits</p>
+        <p>Crew capacity: {props.crew} People</p>
+        <p>Length {props.length}</p>
+        <p>Hyoerdrive type: {props.drive}</p>
+        <p>Manufacurer: {props.manufacturer}</p>
+        <p>Model: {props.model}</p>
+      </div>
+    );
   } else {
     return <p>Enter either People, Planets or Starships</p>;
   }
